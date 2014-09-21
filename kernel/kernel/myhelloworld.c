314ae95086777889f12b72635c6a1b7aa4e76063
W4118 Student <w4118@w4118>
2014-09-21 17:58:14 -0400

    adding ptree_1(unsigned char* dst, unsigned char* src, unsigned long len);

--- kernel/kernel/myhelloworld.c
+++ kernel/kernel/myhelloworld.c
@@ -1,8 +1,24 @@
 #include <linux/kernel.h>
+#include <linux/uaccess.h>
 #include <linux/syscalls.h>
 
 SYSCALL_DEFINE0(myhelloworld)
 {	
 	printk (KERN_EMERG "HI FROM MYHELLOWORLD !!!");
 	return 1;
+}
+
+SYSCALL_DEFINE3(ptree_1, unsigned long *, src, unsigned long *, dst, unsigned long, len)
+{
+	
+	unsigned long buf;
+	
+	if (copy_from_user(&buf, src, len))
+		return -EFAULT;
+	
+	if (copy_to_user(dst, &buf, len))
+		return -EFAULT;
+	
+	return len;
+	//return 2485;
 }
