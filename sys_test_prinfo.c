e8870c55e0d24d825722cc954c01f60e0c373ea0
LawrenceCandes <lfc2128@columbia.edu>
2014-09-27 19:05:46 -0400

    removed index no from process tree

--- sys_test_prinfo.c
+++ sys_test_prinfo.c
@@ -97,8 +97,8 @@ int main(int argc, char **argv)
 
 				for (k = 0; k < noOfTabs; k++)
 					printf("\t");
-				printf("%d, %s, %d, %ld, %d, %d, %d, %ld\n",
-				(i+1), (buf+i)->comm, (buf+i)->pid,
+				printf("%s, %d, %ld, %d, %d, %d, %ld\n",
+				(buf+i)->comm, (buf+i)->pid,
 				(buf+i)->state, (buf+i)->parent_pid,
 				(buf+i)->first_child_pid,
 				(buf+i)->next_sibling_pid, (buf+i)->uid);
