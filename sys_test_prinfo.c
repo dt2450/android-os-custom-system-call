1d0322db7756793100d5cef4f95611af45d3b558
LawrenceCandes <lfc2128@columbia.edu>
2014-09-27 17:19:41 -0400

    tested with system call and printing process tree

--- sys_test_prinfo.c
+++ sys_test_prinfo.c
@@ -22,12 +22,12 @@ struct prinfo {
 
 
 
-void validate_input(int argc, char **argv)
+int validate_input(int argc, char **argv)
 {
 	if (argc != 3) {
 		printf ("Incorrect no. of args\n");
-		printf ("usage: cmd <nr> <buf_sz>");
-		exit(-1);
+		printf ("usage: cmd <nr> <buf_sz>\n");
+		return -1;
 	}
 	
 	printf("argc=%d\n", argc);
@@ -36,38 +36,53 @@ void validate_input(int argc, char **argv)
 	printf("After atoi, nr=%d, buf_sz=%d\n", nr, buf_sz);
 	if (nr < 0 || buf_sz < 0) {
 		printf ("Invalid args\n");
-                printf ("usage: cmd <nr> <buf_sz>");
-		exit(-1);
+                printf ("usage: cmd <nr> <buf_sz>\n");
+		return -1;
 	}
+	return 0;
 }
 
 int main(int argc, char **argv)
 {
-	validate_input(argc, argv);
-
+	int returnValue = validate_input(argc, argv);
+	if(returnValue == -1){
+		return returnValue;
+	}
 	int len = (int) sizeof(struct prinfo);
 	printf("structure len = %d\n", len);
 
-	struct prinfo *buf = (struct prinfo *) malloc(len * buf_sz);
+	struct prinfo *buf = NULL;
+	if(buf_sz > 0){
+		buf = (struct prinfo *) malloc(len * buf_sz);
+	}
 
 	int r = syscall(__NR_ptree, buf, &nr);
 	
 	if (r == -1) {
-		/*err no should be set and we can print error message*/
-		printf("the error is : %s", strerror(errno));
-		exit(-1);
+		printf("the error is : %s\n", strerror(errno));
+		return -1;
 	}
 	
-	int *p = malloc(sizeof(int) * len * 2);
+	printf(" Return value = %d\n", r);
+
+	if(r>nr){
+		printf("\n no of processes is larger than the buffer size!");
+		printf("\n setting the max value of elements to max buffer size : %d\n",nr);
+		r=nr;
+	}
+
+	int *p = malloc(sizeof(int) * r * 2);
 	int i,j;
 	*p = buf->pid;
 	*(p+1) = 0;
 	i=j=1;
-	while (i != ((2*len)-1) ) {
+
+	int k = i;
+
+	while (i != ((2*r)-1) ) {
 		int pid = *(p + i - 1);
 		int noOfTabs = *(p + i);
-		int k = i;
-		for(j=1; j < len; j++) {
+		for(j=1; j < r; j++) {
 			struct prinfo pinfo = *(buf+j);
 			if(pinfo.parent_pid == pid){
 				k+=1;
@@ -78,29 +93,26 @@ int main(int argc, char **argv)
 		}
 		i+=2;	
 	}
-	printf("Process Tree is:\n");
-	for(i=0;i<len;i++){
+
+	printf("\n\n\n\nProcess Tree is:\n");
+	printf("=====================\n");
+	printf("comm, pid, state, parentid, first child pid, next sibling pid, uid\n");
+	for(i=0;i<r;i++){
 		int currentPid = (buf+i)->pid;
-		for(j=0;j<len*2;j+=2){
+		for(j=0;j<r*2;j+=2){
 			if(currentPid == *(p+j)){
 				int noOfTabs = *(p+j+1);
 				int k;
 				for(k=0;k<noOfTabs;k++){
 					printf("\t");
 				}
-				printf("%s,%d,%ld,%d,%d,%d,%ld\n", (buf+i)->comm, (buf+i)->pid, (buf+i)->state,
+				printf("%d, %s,pid=%d,%ld,parent_pid=%d,f_child_pid=%d,%d,%ld\n",(i+1), (buf+i)->comm, (buf+i)->pid, (buf+i)->state,
 					(buf+i)->parent_pid, (buf+i)->first_child_pid, (buf+i)->next_sibling_pid, (buf+i)->uid);
+				break;
 			}
 		}
 	}
 	
 			
-        printf("Values are: ppid: %d pid: %d child_pid: %d sibling_pid: %d",
-                        buf->parent_pid, buf->pid,
-                        buf->first_child_pid,
-                        buf->next_sibling_pid);
-        printf(" state: %lu, uid: %lu, pname: %s\n", buf->state,
-                        buf->uid, buf->comm);
-	printf(" Return value = %d\n", r);
 	return 0;
 }
